2427d7b338f1a7c0f4f3b03ee4faaa55edaf0d5b
Fayash Wahyudha <wahyudhafayash@Fayashs-MacBook-Air.local>
2024-11-28 13:15:13 +0700

    edit: authStore

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,9 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5001"
-    : "https://echora.vercel.app";
+  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
